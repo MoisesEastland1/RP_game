da31c6cb99f5e4ec083a68b34f6fbb747c562626
MoisesEastland1 <meastland977@gmail.com>
2024-02-03 15:09:26 -0600

    refactured code in update function

--- script.js
+++ script.js
@@ -16,16 +16,27 @@ const monsterStats = document.querySelector("#monsterName");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
-const locations = [{}];
+const locations = [
+    {
+        name: "town square",
+        "button text": ["Go to Store", "Go to Cave", "Fight Dragon"],
+        "button fuctions": [ goStore, goCave, fightDragon ],
+        text: "You are in the town square. You see a sign that says \"Store\"."
+    },
+    {
+        name: "store",
+    "button text": ["Buy 10 health (10 gold)","Buy weapon (30 gold)","Go to town square"],
+    "button functions": [buyHealth,buyWeapon,goTown],
+    text: "You enter the store."
+    }
+];
+
+
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update(location) {
-
-}
-
-function goTown() {
     button1.innerText = "Go to store";
     button2.innerText = "Go to cave";
     button3.innerText = "Fight dragon";
@@ -33,6 +44,10 @@ function goTown() {
     button2.onclick = goCave;
     button3.onclick = fightDragon;
     text.innerText = "You are in the town square. You see a sign that says \"Store\"";
+}
+
+function goTown() {
+   
 }
 
 function goStore() {
