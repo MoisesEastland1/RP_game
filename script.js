361d53e8e7e494c9dc96a68ed6b52fa0bdf59ef1
MoisesEastland1 <meastland977@gmail.com>
2024-02-17 05:50:10 -0600

    added currentWeapon concatenation to attack function

--- script.js
+++ script.js
@@ -165,6 +165,7 @@ function goTown() {
 
   function attack(){
     text.innerText = "The "+ monsters[fighting].name +" attack .";
+    text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
   }
 
 
