dc77542d5424b2532ab3936d2d9333ad4cd51806
MoisesEastland1 <meastland977@gmail.com>
2024-02-17 06:18:38 -0600

    added monsterHealth compound assignment to attack function

--- script.js
+++ script.js
@@ -167,6 +167,7 @@ function goTown() {
     text.innerText = "The "+ monsters[fighting].name +" attack .";
     text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
     health -= monsters[fighting].level;
+    monsterHealth -= weapons[currentWeapon].power;
   }
 
 
