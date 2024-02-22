00c315fb9b690a38281b9196909411c20f9cceea
MoisesEastland1 <meastland977@gmail.com>
2024-02-22 16:34:59 -0600

    updated gold and xp innerText

--- script.js
+++ script.js
@@ -184,6 +184,8 @@ function goTown() {
   function defeatMonster(){
     gold += Math.floor(monsters[fighting].level * 6.7)
     xp += monsters[fighting].level
+    goldText.innerText = gold;
+    xpText.innerText = xp;
   }
 
   function lose(){
