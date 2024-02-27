4fa4fe59ee42e544a083940d36caa698d90d10cb
MoisesEastland1 <meastland977@gmail.com>
2024-02-27 17:34:18 -0600

    added for loop

--- script.js
+++ script.js
@@ -271,6 +271,9 @@ function goTown() {
       numbers.push((Math.random() * 11))
     }
     text.innerText = "You picked " + guess + ". Here are the random numbers:\n"
+    for (let i = 0; i < 10; i++){
+      text.innerText += numbers[i] + "\n"
+    }
   }
 
 
